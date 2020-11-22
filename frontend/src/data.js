39286f6632e0977fe11f1a8210abd8fd23f17e77
Philani Ncube <ncbphi001@gmail.com>
2020-11-22 19:56:51 +0200

    commit

---
+++ frontend/src/data.js
@@ -0,0 +1,76 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  products: [
+    {
+      _id: '1',
+      name: 'Nike Slim Shirt',
+      category: 'Shirts',
+      image: '/images/d1.jpg',
+      price: 120,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high qulity product',
+    },
+
+    {
+      _id: '2',
+      name: 'Adidas Fit Shirt',
+      category: 'Shirts',
+      image: '/images/d2.jpg',
+      price: 120,
+      brand: 'Adidas',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high qulity product',
+    },
+
+    {
+      _id: '3',
+      name: 'Lacoste Shirt',
+      category: 'Shirts',
+      image: '/images/d3.jpg',
+      price: 220,
+      brand: 'Lacoste',
+      rating: 4.8,
+      numReviews: 10,
+      description: 'high qulity product',
+    },
+
+    {
+      _id: '4',
+      name: 'Nike Slim Pants',
+      category: 'Pants',
+      image: '/images/p1.jpg',
+      price: 130,
+      brand: ' Nike',
+      rating: 4.7,
+      numReviews: 9,
+      description: 'high qulity product',
+    },
+
+    {
+      _id: '5',
+      name: 'Rebok Dress Pants',
+      category: 'Pants',
+      image: '/images/p2.jpg',
+      price: 130,
+      brand: 'Rebok',
+      rating: 4.7,
+      numReviews: 9,
+      description: 'high qulity product',
+    },
+
+    {
+      _id: '6',
+      name: 'Khaki Pants',
+      category: 'Pants',
+      image: '/images/p3.jpg',
+      price: 130,
+      brand: 'Lee Cooper',
+      rating: 4.3,
+      numReviews: 7,
+      description: 'high qulity product',
+    },
+  ],
+};
